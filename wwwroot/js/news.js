3ce3cf20ecf11cd4722edac7b749071335aae06c
Viktor Bonazza Charlanti <vkbonnazzac@gmail.com>
2023-05-21 12:43:32 -0300

    new animes

--- wwwroot/js/news.js
+++ wwwroot/js/news.js
@@ -1,5 +1,5 @@
-﻿var news = "Novo layout de exibição de animes!\nAproveite ;3";
-let showNewsToUser = 1
+﻿var news = "Mais de 50 novos animes adicionados, incluindo os animes: Oshi no Ko, Konosuba, Fullmetal Alchemist!! :o\nAproveite ;3";
+let showNewsToUser = 0
 //Change between 0 and 1
 
 $(document).ready(function () {
