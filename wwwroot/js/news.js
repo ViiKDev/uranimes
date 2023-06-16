f3f35e0040f40f8340957ee041b2e952d7c4f87b
Viktor Bonazza Charlanti <vkbonnazzac@gmail.com>
2023-06-16 17:34:20 -0300

    news about the functionality

--- wwwroot/js/news.js
+++ wwwroot/js/news.js
@@ -1,5 +1,5 @@
-﻿var news = "Novo anime no ar: \"Araburu Kisetsu\"!\nAproveite ;3";
-let showNewsToUser = "1.0.1"
+﻿var news = "Agora os episódios assistidos receberão uma cor diferente e o próximo será marcado!\nAproveite ;3";
+let showNewsToUser = "1.0.11"
 $(document).ready(function () {
     let recentNews = getCookie('recentNews')
     if (recentNews != showNewsToUser) {
