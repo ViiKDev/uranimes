6894456102e24a27d24f0c39cd0b80d7d97a7661
Viktor Bonazza Charlanti <vkbonnazzac@gmail.com>
2023-07-15 14:46:26 -0300

    sword art online fix

--- wwwroot/js/news.js
+++ wwwroot/js/news.js
@@ -1,5 +1,5 @@
-﻿var news = "Agora os episódios assistidos receberão uma cor diferente e o próximo será marcado!\nAproveite ;3";
-let showNewsToUser = "1.0.11"
+﻿var news = "Temporada 1 de Sword Art Online está novamente disponível!\nAproveite ;3";
+let showNewsToUser = "1.0.12"
 $(document).ready(function () {
     let recentNews = getCookie('recentNews')
     if (recentNews != showNewsToUser) {
