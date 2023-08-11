c4bb4389100f59fbfbed4cccd10020c139089f78
Viktor Bonazza Charlanti <vkbonnazzac@gmail.com>
2023-08-11 10:52:35 -0300

    new anime

--- wwwroot/js/news.js
+++ wwwroot/js/news.js
@@ -1,5 +1,5 @@
-﻿var news = "Temporada 1 de Sword Art Online está novamente disponível!\nAproveite ;3";
-let showNewsToUser = "1.0.12"
+﻿var news = "Anime Hunter X Hunter disponível!\nAproveite ;3";
+let showNewsToUser = "1.0.13"
 $(document).ready(function () {
     let recentNews = getCookie('recentNews')
     if (recentNews != showNewsToUser) {
